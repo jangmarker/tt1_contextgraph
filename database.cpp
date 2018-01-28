90f59790deb52aaa985098c8fe9d659e43e591f0
Jan Marker <jan@jangmarker.de>
2018-01-28 23:22:14 +0100

    Use vector instead of multimap when searching
    improves memory allocation behavior

--- database.cpp
+++ database.cpp
@@ -1,7 +1,11 @@
+#include <algorithm>
+
 #include "database.h"
 
 namespace
 {
+    using WordSimilarity = std::pair<double, WordView>;
+
     double similarity(const Vector& lhs, const Vector& rhs) {
         double topSum = 0.0;
 
@@ -12,26 +16,34 @@ namespace
         return topSum / (lhs.euclideanNorm * rhs.euclideanNorm);
     }
 
-    std::multimap<double, WordView> compare_to_vectors(const Vector& searchVector,
+    std::vector<WordSimilarity> compare_to_vectors(const Vector& searchVector,
                                                    const std::vector<Vector>& vectors)
     {
-        std::multimap<double, WordView> wordsByIncSim;
+        std::vector<WordSimilarity> wordsByIncSim;
+        wordsByIncSim.reserve(vectors.size() - 1);
 
         for (const auto& otherVector : vectors) {
             if (otherVector == searchVector) {
                 continue;
             }
 
-            wordsByIncSim.emplace(similarity(searchVector, otherVector),
-                                  otherVector.word);
+            wordsByIncSim.emplace_back(similarity(searchVector, otherVector),
+                                       otherVector.word);
         }
+
+        std::sort(wordsByIncSim.begin(), wordsByIncSim.end(),
+                  [](const WordSimilarity& lhs, const WordSimilarity& rhs) {
+                      return lhs.first < rhs.first;
+                  }
+        );
+
         return wordsByIncSim;
     }
 
-    template<typename KeyT, typename ValueT>
-    std::vector<ValueT> pick_top(const std::multimap<KeyT, ValueT>& values, std::size_t topCount)
+    template<typename Container>
+    std::vector<WordView> pick_top(const Container& values, std::size_t topCount)
     {
-        std::vector<ValueT> result;
+        std::vector<WordView> result;
         result.reserve(topCount);
         for (auto cur = values.crbegin();
              topCount > 0 && cur != values.crend();
